0e7f5fd23ebbdad6def480bf6e982480f63c55f2
NNIICC <marko.nniicc@gmail.com>
2015-09-14 11:14:30 +0200

    removed unnecessary code

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
 (function() {
-    //form
-    $('.btn-file :file').on('change', function(event) {
-        numFiles = event.target.files.length;
-        label = event.target.files[0].name;
-
-        var input = $(this).parents('.input-group').find(':text'),
-            log = numFiles > 1 ? numFiles + ' files selected' : label;
-
-        if( input.length ) {
-            input.val(log);
-        } else {
-            if( log ) alert(log);
-        }
-    });
-    //form
-    $(".file-text").click(function(event) {
-        event.preventDefault();
-        $('.btn-file :file').trigger('click');
-    });
 
     $(".dropzone").dropzone({
         url: 'upload.php',
