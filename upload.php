089e3e6644c2991467c2e70510f34e14d642d22a
NNIICC <marko.nniicc@gmail.com>
2015-12-03 11:44:07 +0100

    Added php code example

--- upload.php
+++ upload.php
@@ -4,4 +4,8 @@
     print_r($_FILES);
     echo "</pre>";
 
+    if(!move_uploaded_file($_FILES['files']['tmp_name'][0], 'uploads/'.$_FILES['files']['name'][0])){
+        echo "error";
+    }
+    echo "success";
 ?>
